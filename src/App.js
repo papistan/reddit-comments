6d9e045f1c34b6f071fcba95da3682c304cf4ae6
papistan <jpapisan@gmail.com>
2018-04-30 20:57:37 -0700

    Initial setup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Reddit flame wars</h1>
+        <h1>🔥 Flame Wars 🔥</h1>
       </div>
     );
   }
